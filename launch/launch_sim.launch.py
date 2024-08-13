120d7de6e5712531e0e37f94e37b877d8ff0f488
ddrozo <david.rozo31@eia.edu.co>
2024-08-13 14:09:21 -0500

    new

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -36,9 +36,25 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+
+    twist_mux_node = Node(package='twist_mux', 
+                    executable='twist_mux',
+                    parameters=[twist_mux_params,{'use_sim_time': True}],
+                    remappings=[('/cmd_vel_out','/cmd_vel')]
+    )
+
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
+        joystick,
+        twist_mux_node,
         spawn_entity,
     ])
