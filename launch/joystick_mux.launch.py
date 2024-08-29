c99aaf51d197b3cbeae44a034003955c26d9c384
ddrozo <david.rozo31@eia.edu.co>
2024-08-29 16:35:00 -0500

    joystick_mux

---
+++ launch/joystick_mux.launch.py
@@ -0,0 +1,39 @@
+# library to move between files and folders in the O.S.
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+# libraries to define the Launch file and Function
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='f112th_sim_2402_charlie' #<--- CHANGE ME
+
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+
+    twist_mux_node = Node(package='twist_mux', 
+                    executable='twist_mux',
+                    parameters=[twist_mux_params,{'use_sim_time': True}],
+                    remappings=[('/cmd_vel_out','/cmd_vel')]
+    )
+
+    # Launch them all!
+    return LaunchDescription([
+        joystick,
+        twist_mux_node,
+    ])
